0f3e407c4ea7c129dd7ff75b94b330cec88fe3b1
EWinner1 <858564882@qq.com>
2023-04-17 13:45:54 +0800

    Add Redis

--- src/test/java/com/ewinner/hellospringboot/HelloSpringBootApplicationTests.java
+++ src/test/java/com/ewinner/hellospringboot/HelloSpringBootApplicationTests.java
@@ -1,13 +1,28 @@
 package com.ewinner.hellospringboot;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
 
 @SpringBootTest
 class HelloSpringBootApplicationTests {
+
+    @Autowired
+    private RedisTemplate<Object, Object> redisTemplate;
 
     @Test
     void contextLoads() {
     }
 
+    @Test
+    public void TestSetRedisCache(){
+        redisTemplate.boundValueOps("name").set("Justin William");
+    }
+
+    @Test
+    public void TestGetRedisCache(){
+        Object name = redisTemplate.boundValueOps("name").get();
+        System.out.println(name);
+    }
 }
